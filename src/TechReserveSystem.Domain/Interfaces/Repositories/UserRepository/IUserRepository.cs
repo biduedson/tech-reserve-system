31db8e2d898b89c038abe0bc650cc26f22c4e15e
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-01 23:12:11 -0300

    feat(domain): add interface register user repository

--- src/TechReserveSystem.Domain/Interfaces/Repositories/UserRepository/IUserRepository.cs
+++ src/TechReserveSystem.Domain/Interfaces/Repositories/UserRepository/IUserRepository.cs
@@ -7,6 +7,7 @@ namespace TechReserveSystem.Domain.Interfaces.Repositories.UserRepository
         Task<User> GetByIdAsync(Guid id);
         Task<User?> GetByEmailAsync(string email);
         Task<User?> GetByNameAsync(string name);
+        Task<bool> ExistActiveUserWithEmail(string email);
         Task<IEnumerable<User>> GetAllAsync();
         Task AddAsync(User user);
         Task UpdateAsync(User user);
