e77db8d8d7eaef3804c3f3bdbddeb6fe9bc10c10
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-02 19:22:49 -0300

    feat(infrastructure): register infrastructure layer services in Program.cs

--- src/TechReserveSystem.API/Program.cs
+++ src/TechReserveSystem.API/Program.cs
@@ -1,16 +1,22 @@
 using DotNetEnv;
+using TechReserveSystem.Application.Extensions;
+using TechReserveSystem.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
 // Adicione estes serviços antes do Build()
+builder.Services.AddControllers();
+builder.Configuration.AddJsonFile("appsettings.Development", optional: true, reloadOnChange: true);
 
 builder.Configuration.AddEnvironmentVariables();
-
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAuthentication(); // Adiciona serviços de autenticação
 builder.Services.AddAuthorization(); // Adiciona serviços de autorização
 
-builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 
 
