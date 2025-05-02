8281339627ec44b134ae523bb30d65c36dba4cab
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-02 19:12:51 -0300

    feat(Domain): implementing the domain layer user repository interface

---
+++ src/TechReserveSystem.Infrastructure/Data/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using TechReserveSystem.Domain.Entities;
+using TechReserveSystem.Domain.Interfaces.Repositories.UserRepository;
+using TechReserveSystem.Infrastructure.Data.Context;
+
+namespace TechReserveSystem.Infrastructure.Data.Repositories.UserRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserRepository(AppDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<User?> GetById(Guid id) => await _dbContext.Users.FindAsync(id);
+        public async Task<User?> GetByName(string name) => await _dbContext.Users.FindAsync(name);
+        public async Task<User?> GetByEmail(string email) => await _dbContext.Users.FirstOrDefaultAsync(user => user.Email.Contains(email));
+        public async Task<IEnumerable<User>> GetAll() => await _dbContext.Users.ToListAsync();
+        public async Task Add(User user) => await _dbContext.Users.AddAsync(user);
+        public void Update(User user) => _dbContext.Users.Update(user);
+        public void Delete(User user) => _dbContext.Users.Remove(user);
+        public async Task<bool> ExistActiveUserWithEmail(string email) =>
+        await _dbContext.Users.AnyAsync(user => user.Email.Equals(email) && user.IsActive);
+    }
+}
