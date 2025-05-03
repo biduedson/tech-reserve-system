d68bbdc941161346151ad028044486f95f6b6fb3
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-03 19:15:04 -0300

    feat(API): add login user controller

---
+++ src/TechReserveSystem.API/Controllers/Authentication/LoginController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using TechReserveSystem.Application.Interfaces.UseCases.Login;
+using TechReserveSystem.Shared.Communication.Request.Authentication;
+using TechReserveSystem.Shared.Communication.Response.Authentication;
+
+namespace TechReserveSystem.API.Controllers.Authentication
+{
+    public class Logincontroller : ControllerBase
+    {
+        private readonly ILoginUseCase _loginUseCase;
+        public Logincontroller(ILoginUseCase loginUseCase)
+        {
+            _loginUseCase = loginUseCase;
+        }
+        [HttpPost("login")]
+        [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status200OK)]
+
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest request, [FromServices] ILoginUseCase useCase)
+        {
+            var userLogged = await useCase.Execute(request);
+            return Ok(userLogged);
+        }
+    }
+}
