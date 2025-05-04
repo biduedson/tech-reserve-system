b68287f1d5767ea8afd68846f29c96525b3278bf
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-04 15:49:08 -0300

    refact(infrastructure): refact dependenci injection

--- src/TechReserveSystem.Infrastructure/Extensions/DependencyInjectionExtension.cs
+++ src/TechReserveSystem.Infrastructure/Extensions/DependencyInjectionExtension.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using TechReserveSystem.Application.Interfaces.Services.Authentication;
 using TechReserveSystem.Application.Interfaces.Services.Security;
 using TechReserveSystem.Domain.Interfaces.Repositories;
@@ -9,6 +10,7 @@ using TechReserveSystem.Infrastructure.Configuration;
 using TechReserveSystem.Infrastructure.Data.Context;
 using TechReserveSystem.Infrastructure.Data.Repositories;
 using TechReserveSystem.Infrastructure.Data.Repositories.UserRepository;
+using TechReserveSystem.Infrastructure.Security.Authentication;
 using TechReserveSystem.Infrastructure.Services.Authentication;
 using TechReserveSystem.Infrastructure.Services.Security;
 
@@ -18,26 +20,52 @@ namespace TechReserveSystem.Infrastructure.Extensions
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+
+            AddDbContextConfig(services, configuration);
+            AddTokenService(services, configuration);
+            AddPasswordEncryptService(services, configuration);
+            AddRepositories(services, configuration);
+        }
+
+        private static void AddPasswordEncryptService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IPasswordHashService>(provider =>
+            {
+                var additionalKey = configuration.GetValue<string>("Settings:Passwords:AdditionalKey");
+                return new PasswordHashService(configuration, additionalKey!);
+            });
+        }
+
+        private static void AddconfigAuthenticationJwt(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton(provider =>
+            {
+                var config = provider.GetRequiredService<IOptions<JwtSettings>>();
+                AddTokenValidationService.ConfigAuthentication(services, config);
+                return config;
+            });
+        }
+
+        private static void AddTokenService(IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSection = configuration.GetSection("Jwt");
+            services.Configure<JwtSettings>(jwtSection);
+            services.AddSingleton<ITokenService, TokenService>();
+        }
+
+        private static void AddRepositories(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        private static void AddDbContextConfig(IServiceCollection services, IConfiguration configuration)
+        {
             var connectionString = configuration.GetConnectionString("Default");
             var serverVersion = new MySqlServerVersion(new Version());
             services.AddDbContext<AppDbContext>(appDbContextOption =>
             {
                 appDbContextOption.UseMySql(connectionString, serverVersion); ;
-            });
-            var jwtSection = configuration.GetSection("Jwt");
-            services.Configure<JwtSettings>(jwtSection);
-            services.AddSingleton<ITokenService, TokenService>();
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            AddPasswordEncryptService(services, configuration);
-        }
-
-        public static void AddPasswordEncryptService(IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddScoped<IPasswordHashService>(provider =>
-            {
-                var additionalKey = configuration.GetValue<string>("Settings:Passwords:AdditionalKey");
-                return new PasswordHashService(configuration, additionalKey!);
             });
         }
     }
