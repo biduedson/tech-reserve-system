3169a9b8c58724560b5cc4131215e5268dd93b54
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-05 21:13:00 -0300

    feat(infrastructure): add migration EquipmenCategory

--- src/TechReserveSystem.Domain/Entities/EquipmentCategory.cs
+++ src/TechReserveSystem.Domain/Entities/EquipmentCategory.cs
@@ -3,7 +3,8 @@ namespace TechReserveSystem.Domain.Entities
     public class EquipmentCategory
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public ICollection<Equipment> Equipment { get; set; } = new List<Equipment>();
     }
 }
---
+++ src/TechReserveSystem.Infrastructure/Migrations/20250505221002_AddDescriptionToEquipmentCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechReserveSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToEquipmentCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "EquipmentCategories",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "EquipmentCategories");
+        }
+    }
+}
