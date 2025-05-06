8c6cbddc5f09f88869936987ec3c4b14a6e045d3
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-05 21:14:52 -0300

    feat(application): add RegisterEquipmentCategoryUseCase implementation

---
+++ src/TechReserveSystem.API/Controllers/EquipmentCategory/RegisterEquipmentCategoryController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using TechReserveSystem.Application.Interfaces.UseCases.EquipmentCategory;
+using TechReserveSystem.Shared.Communication.Request.EquipmentCategory;
+using TechReserveSystem.Shared.Communication.Response.EquipmentCategory;
+
+namespace TechReserveSystem.API.Controllers.EquipmentCategory
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class RegisterEquipmentCategoryController : ControllerBase
+  {
+    [HttpPost]
+    [ProducesResponseType(typeof(RegisterEquipmentCategoryResponse), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Register(
+      [FromServices] IRegisterEquipmentCategoryUseCase useCases,
+      [FromBody] EquipmentCategoryRegisterRequest request
+    )
+    {
+      var result = await useCases.Execute(request);
+      return Created(string.Empty, result);
+    }
+  }
+}
