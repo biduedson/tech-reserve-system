8c8161de6fb3993dab27e552cac074e830250f53
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-07 21:02:26 -0300

    feat(Shared/communications): add communication messages resx

---
+++ src/Shared/TechReserveSystem.Shared.Communication/Resources/CommunicationMessages.Designer.cs
@@ -0,0 +1,77 @@
+namespace TechReserveSystem.Shared.Communication.Resources
+{
+    using System;
+
+
+    /// <summary>
+    ///   Uma classe de recurso de tipo de alta segurança, para pesquisar cadeias de caracteres localizadas etc.
+    /// </summary>
+    // Essa classe foi gerada automaticamente pela classe StronglyTypedResourceBuilder
+    // através de uma ferramenta como ResGen ou Visual Studio.
+    // Para adicionar ou remover um associado, edite o arquivo .ResX e execute ResGen novamente
+    // com a opção /str, ou recrie o projeto do VS.
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    public class CommunicationMessages
+    {
+
+        private static global::System.Resources.ResourceManager resourceMan;
+
+        private static global::System.Globalization.CultureInfo resourceCulture;
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal CommunicationMessages()
+        {
+        }
+
+        /// <summary>
+        ///   Retorna a instância de ResourceManager armazenada em cache usada por essa classe.
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMan, null))
+                {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TechReserveSystem.Shared.Communication.Resources.CommunicationMessages", typeof(CommunicationMessages).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+
+        /// <summary>
+        ///   Substitui a propriedade CurrentUICulture do thread atual para todas as
+        ///   pesquisas de recursos que usam essa classe de recurso de tipo de alta segurança.
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
+                return resourceCulture;
+            }
+            set
+            {
+                resourceCulture = value;
+            }
+        }
+
+        public static string RESERVATION_SUCCESS
+        {
+            get => ResourceManager.GetString("RESERVATION_SUCCESS", resourceCulture);
+        }
+
+        public static string RESERVATION_CANCELED
+        {
+            get => ResourceManager.GetString("RESERVATION_CANCELED", resourceCulture);
+        }
+
+        public static string EQUIPMENT_NOT_AVAILABLE
+        {
+            get => ResourceManager.GetString("EQUIPMENT_NOT_AVAILABLE", resourceCulture);
+        }
+    }
+}
