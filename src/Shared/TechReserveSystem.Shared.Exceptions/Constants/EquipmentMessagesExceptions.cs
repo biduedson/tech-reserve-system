830e39a11384055eed4e26edddc12d7485d3c2ef
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-09 19:51:34 -0300

    feat(Shared/Exceptions): add constants messages exceptions

---
+++ src/Shared/TechReserveSystem.Shared.Exceptions/Constants/EquipmentMessagesExceptions.cs
@@ -0,0 +1,13 @@
+namespace TechReserveSystem.Shared.Exceptions.Constants
+{
+    public static class EquipmentMessagesExceptions
+    {
+        public const string EQUIPMENT_NOT_FOUND = "EQUIPMENT_NOT_FOUND";
+        public const string EQUIPMENT_ALREADY_REGISTERED = "EQUIPMENT_ALREADY_REGISTERED";
+        public const string EQUIPMENT_ID_EMPTY = "EQUIPMENT_ID_EMPTY";
+        public const string EQUIPMENT_ID_INVALID = "EQUIPMENT_ID_INVALID";
+        public const string EQUIPMENT_NOT_AVAILABLE = "EQUIPMENT_NOT_AVAILABLE";
+        public const string AVAILABLE_QUANTITY_EQUIPMENT_EMPTY = "AVAILABLE_QUANTITY_EQUIPMENT_EMPTY";
+        public const string AVAILABLE_QUANTITY_EQUIPMENT_INVALID = "AVAILABLE_QUANTITY_EQUIPMENT_INVALID";
+    }
+}
