625af0e8441c9b6d3cedeeaba0c0888afdfd9e09
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-10 22:01:56 -0300

    Delete(Shared):delete unusual files

--- src/Shared/TechReserveSystem.Shared.Exceptions/Constants/RequestMessagesExceptions.cs
+++
@@ -1,8 +0,0 @@
-namespace TechReserveSystem.Shared.Exceptions.Constants
-{
-    public static class RequestMessagesExceptions
-    {
-        public const string INVALID_REQUEST_FORMAT = "INVALID_REQUEST_FORMAT";
-        public const string MISSING_REQUIRED_FIELDS = "MISSING_REQUIRED_FIELDS";
-    }
-}
