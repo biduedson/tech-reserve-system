4351db4d9e3ee6684e25453746b28c4435bbc19a
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-10 21:58:58 -0300

    feat(Shared):add responses  http

---
+++ src/TechReserveSystem.Shared/Communication/Response/User/ResponseRegisteredUserJson.cs
@@ -0,0 +1,7 @@
+namespace TechReserveSystem.Shared.Communication.Response.User
+{
+    public class ResponseRegisteredUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
