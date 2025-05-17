2896e2d8033c088c34a4781355e1647123c368a4
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-17 02:46:25 -0300

    feat(Shared):add response base

--- src/TechReserveSystem.Shared/Communication/Response/Response.cs
+++ src/TechReserveSystem.Shared/Communication/Response/Response.cs
@@ -6,22 +6,13 @@ namespace TechReserveSystem.Shared.Communication.Response
         public string Message { get; private set; }
         public T? Data { get; private set; }
 
-        private Response(bool success, string message, T? data)
+        public Response(bool success, string message, T? data)
         {
             Success = success;
             Message = message;
             Data = data;
         }
 
-        public static Response<T> SuccessResponse(T data, string message)
-        {
-            return new Response<T>(true, message, data);
-        }
-
-        public static Response<T> FailureResponse(string message)
-        {
-            return new Response<T>(false, message, default);
-        }
     }
 
 }
