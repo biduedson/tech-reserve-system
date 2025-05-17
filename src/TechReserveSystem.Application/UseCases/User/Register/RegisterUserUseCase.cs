e88bb0a899eed7fece95d0962699dc177acfa8e6
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-17 02:51:33 -0300

    feat(Application): add responseService  to usecases

--- src/TechReserveSystem.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/TechReserveSystem.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,6 +1,8 @@
 
 using TechReserveSystem.Application.Services.Processing.Interfaces;
+using TechReserveSystem.Application.Services.Responses.Interfaces;
 using TechReserveSystem.Shared.Communication.Request.User;
+using TechReserveSystem.Shared.Communication.Response;
 using TechReserveSystem.Shared.Communication.Response.User;
 
 
@@ -9,22 +11,25 @@ namespace TechReserveSystem.Application.UseCases.User.Register
 
     public class RegisterUserUseCase : IRegisterUserUseCase
     {
+        private readonly IResponseService<ResponseRegisteredUserJson> _responseService;
         private readonly IUserProcessingService _userProcessingService;
 
-        public RegisterUserUseCase(IUserProcessingService userProcessingService)
+        public RegisterUserUseCase(IResponseService<ResponseRegisteredUserJson> responseService, IUserProcessingService userProcessingService)
         {
+            _responseService = responseService;
             _userProcessingService = userProcessingService;
         }
 
-        public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
+        public async Task<Response<ResponseRegisteredUserJson>> Execute(RequestRegisterUserJson request)
         {
             var newUser = await _userProcessingService.Register(request);
 
-            return new ResponseRegisteredUserJson
+            var data = new ResponseRegisteredUserJson
             {
                 Name = newUser.Name,
             };
-        }
 
+            return _responseService.Success(data);
+        }
     }
 }
