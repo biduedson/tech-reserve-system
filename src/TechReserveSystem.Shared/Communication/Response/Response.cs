62f8e01081c3d069f6c8e2f937fbc10785b4ab43
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-17 20:16:57 -0300

    feat:(Shared) add optional error field to response model for better API feedback

--- src/TechReserveSystem.Shared/Communication/Response/Response.cs
+++ src/TechReserveSystem.Shared/Communication/Response/Response.cs
@@ -5,15 +5,14 @@ namespace TechReserveSystem.Shared.Communication.Response
         public bool Success { get; private set; }
         public string Message { get; private set; }
         public T? Data { get; private set; }
+        public List<string>? Errors { get; private set; }
 
-        public Response(bool success, string message, T? data)
+        public Response(bool success, string message, T? data, List<string>? errors = null)
         {
             Success = success;
             Message = message;
             Data = data;
+            Errors = errors;
         }
-
     }
-
 }
-
