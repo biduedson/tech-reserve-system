8a349739f20218e2f02963ec330f1693b7b7a7f1
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-18 21:00:12 -0300

    feat(domain): Add Value Objects and Aggregates

---
+++ src/TechReserveSystem.Domain/ValueObjects/ReservationPeriod.cs
@@ -0,0 +1,8 @@
+namespace TechReserveSystem.Domain.ValueObjects
+{
+    public class ReservationPeriod
+    {
+        public DateTime StartDate { get; }
+        public DateTime ExpectedReturnDate { get; }
+    }
+}
