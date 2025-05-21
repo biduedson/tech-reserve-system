37242b36c74ced017d8f77cb5dec945abc5923b6
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-20 22:42:31 -0300

    feat(domain) Add ReservationPeriod value object

--- src/TechReserveSystem.Domain/ValueObjects/ReservationPeriod.cs
+++ src/TechReserveSystem.Domain/ValueObjects/ReservationPeriod.cs
@@ -1,8 +1,35 @@
+using TechReserveSystem.Domain.Constants;
+using TechReserveSystem.Domain.Exceptions;
+
 namespace TechReserveSystem.Domain.ValueObjects
 {
     public class ReservationPeriod
     {
         public DateTime StartDate { get; }
         public DateTime ExpectedReturnDate { get; }
+
+        public ReservationPeriod(DateTime startDate, DateTime expectedReturnDate)
+        {
+            EnsureRules(startDate, expectedReturnDate);
+            StartDate = startDate;
+            ExpectedReturnDate = expectedReturnDate;
+        }
+
+        private void EnsureRules(DateTime startDate, DateTime expectedReturnDate)
+        {
+            var exceptionMessage = (startDate, expectedReturnDate) switch
+            {
+                _ when startDate.Date < DateTime.UtcNow.Date => ReservationPeriodErrorMessages.RESERVATION_PAST_DATE,
+                _ when startDate.Date < DateTime.UtcNow.Date.AddDays(1) => ReservationPeriodErrorMessages.RESERVATION_DATE_TOO_EARLY,
+                _ when startDate.Date > DateTime.UtcNow.Date.AddDays(30) => ReservationPeriodErrorMessages.RESERVATION_MAX_ADVANCE_LIMIT,
+                _ when expectedReturnDate.Date < DateTime.UtcNow.Date => ReservationPeriodErrorMessages.RESERVATION_RETURN_PAST_DATE,
+                _ when expectedReturnDate.Date < startDate.Date => ReservationPeriodErrorMessages.RESERVATION_RETURN_BEFORE_START,
+                _ when expectedReturnDate.Date > startDate.Date => ReservationPeriodErrorMessages.RESERVATION_MUST_BE_SAME_DAY,
+                _ => null
+            };
+
+            if (exceptionMessage is not null)
+                throw new BusinessRuleException(exceptionMessage);
+        }
     }
 }
