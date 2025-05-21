8318fd6c4f84b67e838250a747a66d3a6d891ea2
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-20 22:41:03 -0300

    feat(: Add constants strings messages  and add messages keys

---
+++ src/TechReserveSystem.Domain/Constants/DomainErrorMessages.cs
@@ -0,0 +1,12 @@
+namespace TechReserveSystem.Domain.Constants
+{
+    public static class DomainErrorMessages
+    {
+        public const string USER_NOT_FOUND = "USER_NOT_FOUND";
+        public const string EQUIPMENT_NOT_FOUND = "EQUIPMENT_NOT_FOUND";
+        public const string CATEGORY_NOT_FOUND = "CATEGORY_NOT_FOUND";
+        public const string InvalidQuantity = "InvalidQuantity";
+        public const string InvalidReturnDate = "InvalidReturnDate";
+        public const string InsufficientQuantity = "InsufficientQuantity";
+    }
+}
