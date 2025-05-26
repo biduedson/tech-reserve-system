1a69b20afe9f477680f4ccbc2bfcb9430370fa8b
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-26 02:16:06 -0300

    refact:(domain) add equipment entity constructor

--- src/TechReserveSystem.Domain/Entities/Equipment.cs
+++ src/TechReserveSystem.Domain/Entities/Equipment.cs
@@ -8,6 +8,14 @@ namespace TechReserveSystem.Domain.Entities
         public string Description { get; set; }
         public int AvailableQuantity { get; set; }
         public Guid CategoryId { get; set; }
-        public EquipmentCategory Category { get; set; } = null!;
+
+        public Equipment(string name, string description, int availableQuantity, Guid categoryId)
+        {
+            Name = name;
+            Description = description;
+            AvailableQuantity = availableQuantity;
+            CategoryId = categoryId;
+        }
+
     }
 }
